79f974dcf4d89c2625f8a73f1363cd4279a14414
Bart van Woesik <bartwoesik1@gmail.com>
2024-02-09 13:56:20 +0100

    first code

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Install requirements
+```
+pip install -r requirements.txt
+```
+2. Install package
+```
+pip install -e .
+```
---
+++ src/app/main.py
@@ -0,0 +1,31 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="Example API",
+    description="Kadaster API",
+    version="0.0.1"
+)
+
+
+# Allow all origins, methods and headers
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the root page!"}
+
+
+@app.get("/welcome")
+async def welcome():
+    return {"message": "Welcome user to the API!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5005)
+
