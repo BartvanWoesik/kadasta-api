61a80f997a4d22ce17867e3940be8b63d92f8146
Bart van Woesik <bartwoesik1@gmail.com>
2024-02-09 14:52:26 +0100

    run failed pipeline

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ from app.sum import add_number
 
 
 def test_add_number():
-    assert add_number(1, 2) == 3
+    assert add_number(1, 2) == 4
     assert add_number(-1, 1) == 0
     assert add_number(0, 0) == 0
     assert add_number(10, -5) == 5
