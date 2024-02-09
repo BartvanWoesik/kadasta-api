8c9b8e3a37c226d5e0dd79af34cd1bf531a81665
Bart van Woesik <bartwoesik1@gmail.com>
2024-02-09 14:21:40 +0100

    add unit test for sum

---
+++ src/app/sum.py
@@ -0,0 +1,5 @@
+
+
+
+def add_number(x , y):
+    return x +y
---
+++ tests/test.py
@@ -0,0 +1,9 @@
+
+from app.sum import add_number
+
+
+def test_add_number():
+    assert add_number(1, 2) == 3
+    assert add_number(-1, 1) == 0
+    assert add_number(0, 0) == 0
+    assert add_number(10, -5) == 5
